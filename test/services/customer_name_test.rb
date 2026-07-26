019a76e294797c280707369912acd432b5a39bf9
myst4 <86130282+myst4@users.noreply.github.com>
2026-07-26 14:18:33 -0300

    fix: manejar apellido nulo al formatear el nombre del cliente
    Refs #6

--- test/services/customer_name_test.rb
+++ test/services/customer_name_test.rb
@@ -15,4 +15,14 @@ class CustomerNameTest < ActiveSupport::TestCase
     customer = { first_name: "Ada", last_name: "  Lovelace  " }
     assert_equal "Ada Lovelace", CustomerName.new(customer).full
   end
+
+  test "formats the full name when there is no last name" do
+    customer = { first_name: "Ada" }
+    assert_equal "Ada", CustomerName.new(customer).full
+  end
+
+  test "formats the full name when the last name is nil" do
+    customer = { first_name: "Ada", last_name: nil }
+    assert_equal "Ada", CustomerName.new(customer).full
+  end
 end
