092921d10eb06f1aa2653bf0afbebd876cce2239
myst4 <86130282+myst4@users.noreply.github.com>
2026-07-26 00:57:39 -0300

    fix(slugify): strip punctuation and collapse separators
    Fixes #1

--- slugify.js
+++ slugify.js
@@ -1,5 +1,8 @@
 function slugify(title) {
-  return title.toLowerCase().replace(/ /g, "-");
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
 }
 
 module.exports = { slugify };
