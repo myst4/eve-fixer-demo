f63435d6af4ee79f941f708ea20f37460348ca46
myst4 <86130282+myst4@users.noreply.github.com>
2026-07-26 00:12:17 -0300

    fix(slugify): strip punctuation and collapse separators
    Fixes #1

--- slugify.test.js
+++ slugify.test.js
@@ -5,3 +5,11 @@ const { slugify } = require("./slugify");
 test("lowercases and hyphenates", () => {
   assert.strictEqual(slugify("Hello World"), "hello-world");
 });
+
+test("strips punctuation and collapses separators", () => {
+  assert.strictEqual(slugify("Hello,  World!"), "hello-world");
+});
+
+test("trims leading and trailing hyphens", () => {
+  assert.strictEqual(slugify("--Hello World--"), "hello-world");
+});
